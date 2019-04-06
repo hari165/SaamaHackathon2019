68036b35a32e5af58ee8bf9f528942190fa1a5c7
Harisankar T S <45780504+hari165@users.noreply.github.com>
2019-04-06 18:09:02 +0530

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -1,24 +1,27 @@
 import speech_utils
-import text_utils
+#import text_utils
 import db_utils
-import cx_Oracle
+#import cx_Oracle
 
+username = 'system'
+password = 'password'
+    
+conn = db_utils.db_connection(username, password)
 
 try:
-	username = 'system'
-	password = 'password'
-	conn = sql_to_results.db_connection(username, password)
-
-	continue1=1
-	while continue1!=0:
-		try:
-			raw_text = speech_utils.speech_to_text()
-			sql_text = text_utils.text_to_sql(raw_text)
-			db_utils.db_executor(conn, sql_text)
-			continue1 = int(input('\n\nContinue? (Yes=1, No=0)'))
-		except Exception as e1:
-			print(e1.traceback())
-			print("\n\nRetry")
+    
+    continue1=1
+    while continue1!=0:
+        try:
+            raw_text = speech_utils.speech_to_text()
+            print(raw_text)
+            sql_text = 'select * from emp'#raw_text
+			#sql_text = text_utils.text_to_sql(raw_text)
+            db_utils.db_executor(conn, sql_text)
+            continue1 = int(input('\n\nContinue? (Yes=1, No=0)'))
+        except Exception as e1:
+            print(e1)
+            print("\n\nRetry")
 
 except Exception as e:
 	print(e)
